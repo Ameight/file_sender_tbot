e19d1bd1994213c319ae8ede400c7891c82ab9ad
Ameight <ameightcap@gmail.com>
2021-05-24 00:54:38 +0300

    реализована отправка определённому пользователю

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from aiogram.utils.markdown import text, bold, italic, code, pre
 from aiogram.types import ParseMode, InputMediaPhoto, InputMediaVideo, ChatActions, InputFile
 
 
-from dirscan import listdir, listdir_returnpath, listdir_countfile
+from dirscan import listdir, listdir_returnpath, listdir_countfile, get_file_in_dir, listdir_user, listdir_returnpath_user
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
@@ -20,47 +20,94 @@ dp = Dispatcher(bot)
 
 directory = 'tmp/'
 
-
 #Отправляет список команд в ответ на /start
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply('Привет!\nС помощью команды /help, '
-                        'ты сможешь усзнать список доступных команд!')
+    await message.reply('Привет!\nС помощью команды /help, ты сможешь узнать список доступных команд!')
 
 #Отправляет список команд в ответ на /help
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     msg = text(bold('Я могу ответить на следующие команды:'),
-               '/all - посомотреть список всех отчётов, доступных для просмотра', '/пук', '/пук', sep='\n')
+                    '/all - посомотреть список всех отчётов, доступных для просмотра, доступных вам. ',
+                    message.from_user.username, sep='\n')
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
-#Отправляет список файлов доступных для отправки обратно
+#Отправляет список файлов доступных для отправки определённому пользователю
 @dp.message_handler(commands=['all'])
-async def process_help_command(message: types.Message):
-    msg = text('Файлы доступные для просмотра:\n', listdir(directory) , 'Выберите номер интересующего вас файла.' , sep='\n')
-    await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+async def process_alle_command(message: types.Message):
 
-#Отправка файлов
+    name = message.from_user.username
+    diruser = directory + name
+
+    if(get_file_in_dir(directory, name) == 0): 
+        msg = text('Репозитрий для вас создан!',
+            'У вас пока нет файлов для просмотра, начните пользоваться системой !\n',
+            'Нажимайте кнопку "Синхронизировать с telegram"')
+        await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+    else:
+        if(listdir_user(diruser)):
+            msg = text('Файлы доступные для просмотра:\n', listdir_user(diruser) , 'Выберите номер интересующего вас файла.' , sep='\n')
+            await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+        else:
+            msg = text('Вы авторизированы в системе!', 'У вас пока нет файлов для просмотра.' , sep='\n')
+            await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+
+#Отправка файлов определённому пользователю
 @dp.message_handler()
 async def filter(msg: types.Message):
-    di = msg.text
+
+    di = msg.text                       #Записывает текст сообщения
+    name = msg.from_user.username       #Имя пользователя
+    dirname = directory + name + '/'    #Путь к папке пользователя
+
     if(di.isdigit()):
         tmp = int(msg.text)
-        if (0 < tmp <= listdir_countfile(directory)):
+        if (0 < tmp <= listdir_countfile(dirname)):
+            
             user_id = msg.from_user.id
+
             await bot.send_chat_action(user_id, ChatActions.UPLOAD_DOCUMENT) #Уведомление об отправки файла
             await asyncio.sleep(1)  # скачиваем файл и отправляем его пользователю
-            p = open(listdir_returnpath(directory, tmp), 'rb')
+
+            p = open(listdir_returnpath_user(dirname, tmp), 'rb')
             await bot.send_document(user_id, p,
                                     caption='Этот файл специально для тебя!')
+
         else:
-            p = text('У меня такого файла нет :(', 'Вот список файлов доступных для тебя:' , msg.from_user.username , listdir(directory) , 'Выбери номер интересующего файла.' , sep='\n')
+            p = text('У меня такого файла нет :(', 'Вот список файлов доступных для тебя:' ,
+             msg.from_user.username , listdir(dirname) ,
+              'Выбери номер интересующего файла.' , sep='\n')
             await msg.reply(p, parse_mode=ParseMode.MARKDOWN)
     else:
         message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
                         '\nЯ просто напомню,', 'что есть',
-                        code('команда'), '/help', emojize(':ok_hand:')) 
-    await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
+                        'команда', code('/help'), emojize(':ok_hand:')) 
+        await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
+
+
+#Отправка файлов#
+# @dp.message_handler()
+# async def filter(msg: types.Message):
+#     di = msg.text
+#     if(di.isdigit()):
+#         tmp = int(msg.text)
+#         if (0 < tmp <= listdir_countfile(directory)):
+#             user_id = msg.from_user.id
+#             await bot.send_chat_action(user_id, ChatActions.UPLOAD_DOCUMENT) #Уведомление об отправки файла
+#             await asyncio.sleep(1)  # скачиваем файл и отправляем его пользователю
+#             p = open(listdir_returnpath(directory, tmp), 'rb')
+#             await bot.send_document(user_id, p,
+#                                     caption='Этот файл специально для тебя!')
+#         else:
+#             p = text('У меня такого файла нет :(', 'Вот список файлов доступных для тебя:' , msg.from_user.username , listdir(directory) , 'Выбери номер интересующего файла.' , sep='\n')
+#             await msg.reply(p, parse_mode=ParseMode.MARKDOWN)
+#     else:
+#         message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
+#                         '\nЯ просто напомню,', 'что есть',
+#                         code('команда'), '/help', emojize(':ok_hand:')) 
+#     await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
--- dirscan.py
+++ dirscan.py
@@ -3,11 +3,11 @@ import pathlib
 
 # directory = 'tmp/'
 
-def listdir_base(directory):
-    """ Показывает доступные файлы """
-    files = os.listdir(directory)
-    for i in range(len(files)):
-        print(i, "|", files[i])
+# def listdir_base(directory):
+#     """ Показывает доступные файлы """
+#     files = os.listdir(directory)
+#     for i in range(len(files)):
+#         print(i, "|", files[i])
 
 def listdir(directory):
     """ Показывает доступные файлы """
@@ -19,18 +19,51 @@ def listdir(directory):
         tmp = tmp + str(counter + 1) + " | " + files[i] + '\n'
     return tmp
     
-def listdir_returnpath(directory, number):
-    """ Возвращает путь к файлу """
-    if(number > 0):
-        file_path = directory
-        number = number - 1
-        files = os.listdir(directory)
-        for i in range(len(files)):
-            if(i == number):
-                file_path = file_path + files[i]
-                return file_path
+# def listdir_returnpath(directory, number):
+#     """ Возвращает путь к файлу """
+#     if(number > 0):
+#         file_path = directory
+#         number = number - 1
+#         files = os.listdir(directory)
+#         for i in range(len(files)):
+#             if(i == number):
+#                 file_path = file_path + files[i]
+#                 return file_path
 
 def listdir_countfile(directory):
     """ Показывает количесво доступных файлов """
     files = os.listdir(directory)
     return len(files)
+
+#Создание репозиотрия пользователя, если он уже есть - вернет 1
+# если его нет, создаёт его и возвращает 0
+def get_file_in_dir(directory, name):
+    """ Создание и проверка репозиотрия пользователя """
+    nmdir= directory + name
+    if os.path.exists(nmdir):
+        return 1
+    else:
+        os.mkdir(nmdir)
+        return 0
+
+def listdir_user(diruser):
+    """ Показывает доступные файлы """
+    files = os.listdir(diruser)
+    tmp = ""
+    counter=0
+    for i in range(len(files)):
+        counter=i
+        tmp = tmp + str(counter + 1) + " | " + files[i] + '\n'
+    return tmp
+
+# Возвращает путь к файлу пользователя по его запросу
+def listdir_returnpath_user(diruser, number):
+    """ Возвращает путь к файлу пользователя"""
+    if(number > 0): 
+        file_path = diruser
+        number = number - 1
+        files = os.listdir(diruser)
+        for i in range(len(files)):
+            if(i == number):
+                file_path = file_path + files[i]
+                return file_path
